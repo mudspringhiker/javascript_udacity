0723d8d330f86a5480057a7723463c99b847f7e7
Alona Varshal <thirdwave72@gmail.com>
2017-10-20 14:39:22 -0500

    Add Quiz 4-3

--- 4-3_countdown_liftoff.js
+++ 4-3_countdown_liftoff.js
@@ -8,9 +8,35 @@
 
 var time = 60;
 
-while (time > 0) { 
-	if (time === 50) {
-		console.log("Orbiter transfers from ground to internal power")
-	} 
-	console.log("T-" + time + seconds);
+while (time >= 0) { 
+
+	switch (time) {
+		case 50:
+			console.log("Orbiter transfers from ground to internal power");
+			break;
+		case 31:
+			console.log("Ground launch sequencer us go for auto sequence start");
+			break;
+		case 16:
+			console.log("Activate launch pad sound suppression system");
+			break;
+		case 10:
+			console.log("Activate main engine hydrogen burnoff system");
+			break;
+		case 6:
+			console.log("Main engine start");
+			break;
+		case 0:
+			console.log("Solid rocket booster ignition and liftoff!");
+			break;
+		default: 
+			console.log("T-" + time + " seconds");
+	}
+	time = time - 1;
 }
+
+
+
+
+	
+
